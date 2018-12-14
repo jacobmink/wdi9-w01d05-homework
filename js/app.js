1206f3b99f5556f6cf51249aa6c0868026bc9610
Jacob Mink <jacobmink@gmail.com>
2018-12-14 11:29:16 -0700

    Commit 1 - Q & A

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// VARIABLE AND DATATYPES
+// 1) Assigning a value to a variable: [let, const] variableName = value;
+// 2) Change value of variable: variableName = newValue;
+// 3) Assign existing var to new var: newVariable = existingVariable;
+// 4) Declare: let variableName;
+//      Assign: let variableName = value;
+//      Define: Another term for assign;
+// 5) Pseudocode is writing out what you want the program to do on the computer
+//      using a proper flow, but not using real coding syntax.
+// 6) 80% thinking, 20% coding (approximately)
