7059bd369aea33b90b623cb5103d3fc2cf565329
Jacob Mink <jacobmink@gmail.com>
2018-12-14 11:34:11 -0700

    Commit 2 - Strings

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 // VARIABLE AND DATATYPES
+
+// Q + A
 // 1) Assigning a value to a variable: [let, const] variableName = value;
 // 2) Change value of variable: variableName = newValue;
 // 3) Assign existing var to new var: newVariable = existingVariable;
@@ -8,3 +10,17 @@
 // 5) Pseudocode is writing out what you want the program to do on the computer
 //      using a proper flow, but not using real coding syntax.
 // 6) 80% thinking, 20% coding (approximately)
+
+// Strings
+// 1)
+let firstVariable = 'Hello World';
+firstVariable = 99;
+let secondVariable = firstVariable;
+secondVariable = 'Whats up';
+// firstVariable still equals 99
+
+// 2)
+const yourName = 'Jake Mink';
+const sentence = 'Hello, my name is ';
+const full = sentence + yourName;
+console.log(full);
