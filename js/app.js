cf61126f900172c63718a873211544235eb7fe59
Jacob Mink <jacobmink@gmail.com>
2018-12-15 12:23:32 -0700

    Commit 34 - Loops

--- js/app.js
+++ js/app.js
@@ -452,3 +452,13 @@ user.friend.purchased.push('The One Ring');
 user.friend.purchased.push('A latte');
 // 7)
 console.log(user.friend.purchased[1]);
+
+// Loops
+// 1)
+for (let i = 0; i < user.purchased.length; i++) {
+    console.log(user.purchased[i]);
+}
+// 2)
+for (let i = 0; i < user.friend.purchased.length; i++) {
+    console.log(user.friend.purchased[i]);
+}
